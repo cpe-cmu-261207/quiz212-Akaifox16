5e1bfadfeb38b26579fdfade0eaf8ba157033b53
Akaifox16 <54855870+Akaifox16@users.noreply.github.com>
2021-02-02 13:42:57 +0700

    completed (maybe)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { useState } from "react";
 function App() {
   const persons = [
+    {
+      name: "Kamonpat Sunthonpong",
+      gender: "male",
+      age: "20"
+    },
     {
       name: "Bob",
       gender: "male",
@@ -11,33 +17,54 @@ function App() {
       age: "20"
     }
   ];
+  const [id, setID] = useState(620610771);
+  const change = (v) => {
+    let current = id;
+    setID(id + v);
+  };
   return (
     <div class="ml-2">
       {/* Code me please! */}
       <div class="mb-4">
         <h3 class="title is-3">ID Counter</h3>
-        <p>YOUR ID HERE</p>
-        <button>-</button>
-        <button>reset</button>
-        <button>+</button>
+        <p>{id}</p>
+        <button
+          onClick={() => {
+            change(-1);
+          }}
+        >
+          -
+        </button>
+        <button onClick={() => setID(620610771)}>reset</button>
+        <button
+          onClick={() => {
+            change(1);
+          }}
+        >
+          +
+        </button>
       </div>
 
       {/* Convert me to a component! */}
       <h3 class="title is-3">Person List</h3>
-      <table class="table is-bordered mb-3">
-        <tbody>
-          <tr>
-            <th>Name</th>
-            <th>Gender</th>
-            <th>Age</th>
-          </tr>
-          <tr>
-            <td>Bob</td>
-            <td>male</td>
-            <td>50</td>
-          </tr>
-        </tbody>
-      </table>
+      {persons.map((person) => {
+        return (
+          <table class="table is-bordered mb-3">
+            <tbody>
+              <tr>
+                <th>Name</th>
+                <th>Gender</th>
+                <th>Age</th>
+              </tr>
+              <tr>
+                <td>{person.name}</td>
+                <td>{person.gender}</td>
+                <td>{person.age}</td>
+              </tr>
+            </tbody>
+          </table>
+        );
+      })}
     </div>
   );
 }
